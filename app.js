16d87fb838689c1d8dd76373452b28352308323e
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-20 20:46:30 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function btnOn(){
+    var bulb1 = document.querySelector(".bulbOff")
+    bulb1.setAttribute("src","on-removebg-preview (1).png")
+}
+function btnOFF(){
+    var bulb1 = document.querySelector(".bulbOff")
+    bulb1.setAttribute("src","off-removebg-preview (1).png")
+}
